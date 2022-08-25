9d4250546d397767f5640d027c47d21c8f40a313
Raja Oad <rajarajput2003@gmail.com>
2022-08-25 23:24:36 +0500

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,24 @@
+function menu() {
+    document.getElementById('pizza').style.display="block";
+    document.getElementById('burger').style.display="none";
+    document.getElementById('drinks').style.display="none";
+}
+function menu2() {
+    document.getElementById('pizza').style.display="none";
+    document.getElementById('burger').style.display="block";
+    document.getElementById('drinks').style.display="none";
+}
+function menu3() {
+    document.getElementById('pizza').style.display="none";
+    document.getElementById('burger').style.display="none";
+    document.getElementById('drinks').style.display="block";
+}
+
+function navon() {
+    document.getElementById('nav-bar').style.display="block";
+    document.getElementById('ham').style.display="none";
+}
+function navoff() {
+    document.getElementById('nav-bar').style.display="none";
+    document.getElementById('ham').style.display="block";
+}
